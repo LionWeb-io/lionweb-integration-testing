5b9ced366201784bf42b371c65b2d17bdb61e230
Niko Stotz <niko@nikostotz.de>
2025-06-16 23:24:28 +0200

    cleanup

--- src/cs/LionWeb.Integration.WebSocket.Tests/WebSocketTests.cs
+++ src/cs/LionWeb.Integration.WebSocket.Tests/WebSocketTests.cs
@@ -15,6 +15,11 @@ namespace LionWeb.Integration.WebSocket.Tests;
 [TestClass]
 public class WebSocketTests : WebSocketClientTestBase
 {
+    private static readonly IVersion2023_1 _lionWebVersion = LionWebVersions.v2023_1;
+    private static readonly List<Language> _languages = [ShapesLanguage.Instance, _lionWebVersion.BuiltIns, _lionWebVersion.LionCore];
+    private const string IpAddress = "localhost";
+    private const int Port = 42424;
+
     [TestMethod]
     public void bla()
     {
@@ -65,9 +70,6 @@ public class WebSocketTests : WebSocketClientTestBase
     [TestMethod, Timeout(3000)]
     public async Task Communication()
     {
-        // var server = Server;
-        // server.Received += (sender, msg) => Console.WriteLine($"server received: {msg}");
-
         var clientA = new WebSocketClient("A");
         clientA.Receive += (sender, msg) => Console.WriteLine($"client A received: {msg}");
 
@@ -78,16 +80,9 @@ public class WebSocketTests : WebSocketClientTestBase
         var port = 42424;
         await clientA.ConnectToServer($"ws://{ipAddress}:{port}");
         await clientB.ConnectToServer($"ws://{ipAddress}:{port}");
-        // await server.Send("hello from server");
         await clientA.Send("hello from client A");
-        // await server.Send("bye from server");
         Thread.Sleep(100);
     }
-
-    static IVersion2023_1 lionWebVersion = LionWebVersions.v2023_1;
-    static List<Language> languages = [ShapesLanguage.Instance, lionWebVersion.BuiltIns, lionWebVersion.LionCore];
-    private static readonly string _ipAddress = "localhost";
-    private static readonly int _port = 42424;
 
     [TestMethod, Timeout(6000)]
     public async Task Model()
@@ -116,14 +111,17 @@ public class WebSocketTests : WebSocketClientTestBase
         Debug.WriteLine($"clientA Documentation {aPartition.Documentation.PrintIdentity()}");
         Debug.WriteLine($"clientB Documentation {bPartition.Documentation.PrintIdentity()}");
 
+        bPartition.Documentation.Text = "bye there";
+        aClient.WaitForCount(4);
+
         AssertEquals([aPartition], [bPartition]);
     }
 
     private static async Task<LionWebClient> ConnectWebSocket(Geometry clientAClone, string name)
     {
         var webSocket = new WebSocketClient(name);
-        var lionWeb = new LionWebClient(lionWebVersion, languages, $"client_{name}", clientAClone, webSocket);
-        await webSocket.ConnectToServer($"ws://{_ipAddress}:{_port}");
+        var lionWeb = new LionWebClient(_lionWebVersion, _languages, $"client_{name}", clientAClone, webSocket);
+        await webSocket.ConnectToServer($"ws://{IpAddress}:{Port}");
         await lionWeb.Send(new SignOnRequest("2025.1", IdUtils.NewId(), null));
         lionWeb.WaitForCount(1);
         return lionWeb;
