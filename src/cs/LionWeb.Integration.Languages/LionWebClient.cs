52ed6a7385f3e197ff5cf70ab1f961b2cdd7557a
Niko Stotz <niko@nikostotz.de>
2025-06-20 15:58:05 +0200

    moved delta messages to separate namespace

--- src/cs/LionWeb.Integration.Languages/LionWebClient.cs
+++ src/cs/LionWeb.Integration.Languages/LionWebClient.cs
@@ -1,14 +1,36 @@
-﻿using System.Collections.Concurrent;
+﻿// Copyright 2025 LionWeb Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-FileCopyrightText: 2025 LionWeb Project
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using LionWeb.Core;
 using LionWeb.Core.M1;
 using LionWeb.Core.M1.Event.Partition;
 using LionWeb.Core.M3;
 using LionWeb.Core.Serialization;
-using ParticipationId = string;
-using CommandId = string;
+using LionWeb.Core.Serialization.Delta;
+using LionWeb.Core.Serialization.Delta.Command;
+using LionWeb.Core.Serialization.Delta.Event;
+using LionWeb.Core.Serialization.Delta.Query;
 
 namespace LionWeb.Integration.Languages;
+
+using ParticipationId = NodeId;
+using CommandId = NodeId;
 
 public interface IDeltaClientConnector
 {
