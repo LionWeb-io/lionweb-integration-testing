80931551e03ec05aaed98378767cde21a7936bd6
Meinte Boersma <meinte.boersma@gmail.com>
2025-06-20 15:28:17 +0200

    get Deno-powered scripts working again

--- src/m3-instances.test.ts
+++ src/m3-instances.test.ts
@@ -1,7 +1,8 @@
 import {assertEquals} from "./deps.ts"
 
-import {deserializeLanguages, SerializationChunk} from "lionweb-core"
-import {sortedSerialization, readFileAsJson} from "lionweb-utilities"
+import {deserializeLanguages} from "lionweb-core"
+import {LionWebJsonChunk} from "lionweb-json"
+import {sortedSerializationChunk, readFileAsJson} from "lionweb-utilities"
 
 import {fromRoot, pathOfSerialization} from "./config.ts"
 
@@ -10,13 +11,13 @@ Deno.test("M3 instances (Deno)", async (tctx) => {
 
     await tctx.step("check whether Java serialization of LionCore/M3 deserializes in TypeScript impl. (no assertions)", async () => {
         const serializationJava = await readFileAsJson(fromRoot(pathOfSerialization("m3", "Java")))
-        /* const deserializationJava = */ deserializeLanguages(serializationJava as SerializationChunk)
+        /* const deserializationJava = */ deserializeLanguages(serializationJava as LionWebJsonChunk)
     })
 
     await tctx.step("check whether Java and TypeScript serializations match", async () => {
-        const serializationJava = await readFileAsJson(fromRoot(pathOfSerialization("m3", "Java"))) as SerializationChunk
-        const serializationTypeScript = await readFileAsJson(fromRoot(pathOfSerialization("m3", "TypeScript"))) as SerializationChunk
-        assertEquals(sortedSerialization(serializationJava), sortedSerialization(serializationTypeScript))
+        const serializationJava = await readFileAsJson(fromRoot(pathOfSerialization("m3", "Java"))) as LionWebJsonChunk
+        const serializationTypeScript = await readFileAsJson(fromRoot(pathOfSerialization("m3", "TypeScript"))) as LionWebJsonChunk
+        assertEquals(sortedSerializationChunk(serializationJava), sortedSerializationChunk(serializationTypeScript))
     })
 
 })
