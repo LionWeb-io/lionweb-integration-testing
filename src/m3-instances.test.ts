ba9b62b58189bde7bb3c5cfa1bc98cbcc7bd6a0a
Meinte Boersma <meinte.boersma@gmail.com>
2025-06-30 14:09:41 +0200

    the specification repo used to be the organization repo
      + add 2 unit tests to compare against specification

--- src/m3-instances.test.ts
+++ src/m3-instances.test.ts
@@ -9,15 +9,30 @@ import {fromRoot, pathOfSerialization} from "./config.ts"
 
 Deno.test("M3 instances (Deno)", async (tctx) => {
 
+    const javaSerializationPath = fromRoot(pathOfSerialization("m3", "Java"));
+    const javaSerialization = sortedSerializationChunk(await readFileAsJson(javaSerializationPath) as LionWebJsonChunk)
+    const tsSerializationPath = fromRoot(pathOfSerialization("m3", "TypeScript"));
+    const tsSerialization = sortedSerializationChunk(await readFileAsJson(tsSerializationPath) as LionWebJsonChunk)
+    const specSerializationPath = fromRoot(pathOfSerialization("spec", "specification"));
+    const specSerialization = sortedSerializationChunk(await readFileAsJson(specSerializationPath) as LionWebJsonChunk)
+
     await tctx.step("check whether Java serialization of LionCore/M3 deserializes in TypeScript impl. (no assertions)", async () => {
-        const serializationJava = await readFileAsJson(fromRoot(pathOfSerialization("m3", "Java")))
-        /* const deserializationJava = */ deserializeLanguages(serializationJava as LionWebJsonChunk)
+        /* const deserializationJava = */ deserializeLanguages(javaSerialization)
     })
 
-    await tctx.step("check whether Java (=\x1b[1;31mActual\x1b[0m/left) and TypeScript (=\x1b[1;32mExpected\x1b[0m/right) serializations match", async () => {
-        const serializationJava = await readFileAsJson(fromRoot(pathOfSerialization("m3", "Java"))) as LionWebJsonChunk
-        const serializationTypeScript = await readFileAsJson(fromRoot(pathOfSerialization("m3", "TypeScript"))) as LionWebJsonChunk
-        assertEquals(sortedSerializationChunk(serializationJava), sortedSerializationChunk(serializationTypeScript))
+    const red = (text: string) => `\x1b[1;31m${text}\x1b[0m`
+    const green = (text: string) => `\x1b[1;32m${text}\x1b[0m`
+
+    await tctx.step(`check whether Java (=${red("Actual")}/left, path=${javaSerializationPath}) and TypeScript (=${green("Expected")}/right, path=${tsSerializationPath}) serialization of LionCore/M3 match`, async () => {
+        assertEquals(javaSerialization, tsSerialization)
+    })
+
+    await tctx.step(`check whether Java (=${red("Actual")}/left, path=${javaSerializationPath}) serialization of LionCore/M3 matches with the specification (=${green("Expected")}/right), path=${specSerializationPath}`, async () => {
+        assertEquals(javaSerialization, specSerialization)
+    })
+
+    await tctx.step(`check whether TypeScript (=${red("Actual")}/left, path=${tsSerializationPath}) serialization of LionCore/M3 matches with the specification (=${green("Expected")}/right, path=${specSerializationPath})`, async () => {
+        assertEquals(tsSerialization, specSerialization)
     })
 
 })
