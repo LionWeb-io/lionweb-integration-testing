4b256959aade18b6796fb388ae4f935f9ca271e5
Erkan Diken <erkan@f1re.io>
2025-07-21 09:49:49 +0200

    Add test for adding a property in WebSocket server tests

---
+++ src/cs/LionWeb.Integration.WebSocket.Tests/Server/PropertyServerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using LionWeb.Core.M1.Event;
+using LionWeb.Integration.Languages.Generated.V2023_1.TestLanguage.M2;
+using LionWeb.Integration.WebSocket.Server;
+using LionWeb.Protocol.Delta.Repository;
+
+namespace LionWeb.Integration.WebSocket.Tests.Server;
+
+[TestFixture(ClientProcesses.CSharp)]
+public class PropertyServerTests(params ClientProcesses[] clientProcesses) : WebSocketServerTestBase(clientProcesses)
+{
+    /// <summary>
+    /// Adds a property
+    /// </summary>
+    [Test]
+    public void AddProperty()
+    {
+        _webSocketServer = new WebSocketServer(_lionWebVersion) { Languages = _languages };
+        _webSocketServer.StartServer(IpAddress, Port);
+
+        var serverPartition = new DataTypeTestConcept("a");
+        Debug.WriteLine($"Server partition: {serverPartition.PrintIdentity()}");
+
+        var lionWebServer =
+            new LionWebTestRepository(_lionWebVersion, _languages, "server", serverPartition, _webSocketServer);
+
+        StartClient("A", "SignOn", "AddStringValue_0_1");
+
+        lionWebServer.WaitForReceived(2);  
+
+        var expected = new DataTypeTestConcept("a")
+        {
+            StringValue_0_1 = "new property"
+        };
+
+        AssertEquals(expected, serverPartition);
+    }
+}
