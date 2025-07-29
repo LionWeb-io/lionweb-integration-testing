07237846cd21de601cf4c6e712b7af8e966a884f
Meinte Boersma <meinte.boersma@gmail.com>
2025-07-29 13:04:08 +0200

    replace wget with curl

--- download-delta.schema.json.sh
+++ download-delta.schema.json.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 
-wget https://raw.githubusercontent.com/LionWeb-io/specification/refs/heads/main/delta/delta.schema.json
+curl -O https://raw.githubusercontent.com/LionWeb-io/specification/refs/heads/main/delta/delta.schema.json
