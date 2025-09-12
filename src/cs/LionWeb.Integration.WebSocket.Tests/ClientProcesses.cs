585adfc46dccbe1ecaf7861ca6e69a894131088d
Niko Stotz <niko@nikostotz.de>
2025-09-12 17:20:35 +0200

    use npx to call TypeScript client

--- src/cs/LionWeb.Integration.WebSocket.Tests/ClientProcesses.cs
+++ src/cs/LionWeb.Integration.WebSocket.Tests/ClientProcesses.cs
@@ -16,6 +16,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using LionWeb.Integration.WebSocket.Client;
 
 namespace LionWeb.Integration.WebSocket.Tests;
@@ -32,7 +33,7 @@ public static class ClientProcessesExtensions
         IEnumerable<string> tasks, out string readyTrigger, out string errorTrigger) => process switch
     {
         ClientProcesses.CSharp => CSharpClient(name, partitionType, port, tasks, out readyTrigger, out errorTrigger),
-        ClientProcesses.Ts => TsClient(name, port, tasks, out readyTrigger, out errorTrigger),
+        ClientProcesses.Ts => TsClient(name, partitionType, port, tasks, out readyTrigger, out errorTrigger),
         _ => throw new ArgumentOutOfRangeException(nameof(process), process, null)
     };
 
@@ -59,20 +60,30 @@ public static class ClientProcessesExtensions
         return result;
     }
 
-    private static Process TsClient(string clientId, int port, IEnumerable<string> tasks, out string trigger,
+    private static Process TsClient(string clientId, string partitionType, int port, IEnumerable<string> tasks,
+        out string trigger,
         out string errorTrigger)
     {
+        string fileName = "npx";
+        string argumentPrefix = "";
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            fileName = "powershell";
+            argumentPrefix = "npx ";
+        }
+        
         var result = new Process();
-        result.StartInfo.FileName = "node";
+        result.StartInfo.FileName = fileName;
         // Note: the following line means that it's assumed that
         //  1) the lionweb-typescript repo is located right next to the lionweb-integration-testing repo,
         //  2) that the latter repo has been checked out on the delta-protocol-impl branch,
         //  3) and that it's been built entirely successfully.
-        result.StartInfo.WorkingDirectory =
-            $"{Directory.GetCurrentDirectory()}/../../../../../../../lionweb-typescript/packages/deltas-websocket";
+        // result.StartInfo.WorkingDirectory =
+        //     $"{Directory.GetCurrentDirectory()}/../../../../../../../lionweb-typescript/packages/deltas-websocket";
         // cwd is assumed to be: <LionWeb dir.>/lionweb-integration-testing/src/cs/LionWeb.Integration.WebSocket.Tests/bin/Debug/net8.0
         // (hence 7x ../)
-        result.StartInfo.Arguments = $"dist/cli/client.js {port} {clientId} {string.Join(",", tasks)}";
+        // result.StartInfo.Arguments = $"dist/cli/client.js {port} {clientId} {string.Join(",", tasks)}";
+        result.StartInfo.Arguments = $"{argumentPrefix}--package=@lionweb/delta-protocol-test-cli@0.7.0-beta.13 cli-client {port} {clientId} {partitionType} {string.Join(",", tasks)}";
         result.StartInfo.UseShellExecute = false;
         trigger = "LionWeb delta protocol client";
         errorTrigger = "Error";
