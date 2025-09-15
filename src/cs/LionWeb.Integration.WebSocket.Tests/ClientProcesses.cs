0303e1670ddccd5b124f1ccde456809834d001d2
Niko Stotz <niko@nikostotz.de>
2025-09-15 17:42:12 +0200

    For Debug builds, use LionWeb C#/TypeScript libraries from local directory.
    For Release builds (and in pipeline), use LionWeb C#/TypeScript libraries from package management.

--- src/cs/LionWeb.Integration.WebSocket.Tests/ClientProcesses.cs
+++ src/cs/LionWeb.Integration.WebSocket.Tests/ClientProcesses.cs
@@ -60,45 +60,83 @@ public static class ClientProcessesExtensions
         return result;
     }
 
-    /// <remarks>
-    /// This method assumes that
-    /// <list type="number">
-    /// <item>environment variable <see cref="TsDeltaCliVersionEnvironmentVariable"/> is set.</item>
-    /// <item><c>npx --package=@lionweb/delta-protocol-test-cli@{TsDeltaCliVersion}</c> executes successfully and within the timeout.
-    ///     Execute it once before running tests to make sure everything is already downloaded and cached locally.</item>
-    /// </list>
-    /// </remarks>
+    #region TypeScript client
+
     private static Process TsClient(string clientId, string partitionType, int port, IEnumerable<string> tasks,
         out string trigger,
         out string errorTrigger)
     {
-        // On Windows, we need to call `powershell npx` instead of `npx`.
-        string fileName = "npx";
-        string argumentPrefix = "";
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            fileName = "powershell";
-            argumentPrefix = "npx ";
-        }
+        var cmdLine = $"{port} {clientId} {partitionType} {string.Join(",", tasks)}";
 
-        var result = new Process();
-        result.StartInfo.FileName = fileName;
-        result.StartInfo.Arguments =
-            $"{argumentPrefix}--package=@lionweb/delta-protocol-test-cli@{TsDeltaCliVersion} cli-client {port} {clientId} {partitionType} {string.Join(",", tasks)}";
+// Accesses `<DefineConstants>USE_LION_WEB_PACKAGES</DefineConstants>` from .csproj 
+#if USE_LION_WEB_PACKAGES
+        var result = TsNpxPackageClient(cmdLine);
+#else
+        var result = TsRelativeDirectoryClient(cmdLine);
+#endif
+
         result.StartInfo.UseShellExecute = false;
         trigger = "LionWeb delta protocol client";
         errorTrigger = "Error";
         return result;
     }
 
-    private const string TsDeltaCliVersionEnvironmentVariable = "TS_DELTA_CLI_VERSION";
+    /// <remarks>
+    /// This method assumes that
+    /// <list type="number">
+    ///  <item>the lionweb-typescript repo is located right next to the lionweb-integration-testing repo,</item>
+    ///  <item>that the latter repo has been checked out on the delta-protocol-impl branch,</item>
+    ///  <item>and that it's been built entirely successfully.</item>
+    /// </list>
+    /// </remarks>
+    private static Process TsRelativeDirectoryClient(string cmdLine)
+    {
+        var result = new Process();
+        result.StartInfo.FileName = "node";
+        result.StartInfo.WorkingDirectory =
+            $"{Directory.GetCurrentDirectory()}/../../../../../../../lionweb-typescript/packages/deltas-websocket";
+        // cwd is assumed to be: <LionWeb dir.>/lionweb-integration-testing/src/cs/LionWeb.Integration.WebSocket.Tests/bin/Debug/net8.0
+        // (hence 7x ../)
+        result.StartInfo.Arguments = $"dist/cli/client.js {cmdLine}";
+
+        return result;
+    }
 
     /// <remarks>
-    /// There's a
-    /// <a href="https://www.jetbrains.com/help/rider/Reference__Options__Tools__Unit_Testing__Test_Runner.html#environment-variables">special place</a>
-    /// for setting test environment variables in Rider. 
+    /// This method assumes that
+    /// <list type="number">
+    /// <item><i>Directory.Packages.props</i> property <c>LionWebTsVersion</c> is set.</item>
+    /// <item><c>npx --package=@lionweb/delta-protocol-test-cli@{TsDeltaCliVersion}</c> executes successfully and within the timeout.
+    ///     Execute it once before running tests to make sure everything is already downloaded and cached locally.</item>
+    /// </list>
     /// </remarks>
-    private static string TsDeltaCliVersion =>
-        Environment.GetEnvironmentVariable(TsDeltaCliVersionEnvironmentVariable) ??
-        throw new ArgumentException($"environment variable {TsDeltaCliVersionEnvironmentVariable} not set");
+    private static Process TsNpxPackageClient(string cmdLine)
+    {
+        var result = new Process();
+        var npxArg = $"--package=@lionweb/delta-protocol-test-cli@{TsDeltaCliVersion} cli-client";
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            // On Windows, we need to call `powershell npx` instead of `npx`.
+            result.StartInfo.FileName = "powershell";
+            result.StartInfo.Arguments = $"npx {npxArg} {cmdLine}";
+        }
+        else
+        {
+            result.StartInfo.FileName = "npx";
+            result.StartInfo.Arguments = $"${npxArg} ${cmdLine}";
+        }
+
+        return result;
+    }
+
+    internal static string TsDeltaCliVersion =>
+        typeof(ClientProcesses)
+            .Assembly
+            .GetCustomAttributes(typeof(AssemblyConfigurationAttribute), false)
+            .OfType<AssemblyConfigurationAttribute>()
+            .First(cfg => cfg.Key == "LionWebTsVersion")
+            .Value;
+
+    #endregion
 }
