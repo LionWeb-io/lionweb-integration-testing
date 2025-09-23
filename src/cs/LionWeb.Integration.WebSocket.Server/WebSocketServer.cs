9ba5e42eaa631fbfc2559ee0a88409f80677e179
Erkan Diken <erkan@f1re.io>
2025-09-23 13:26:29 +0200

    Remove unnecessary argument validation

--- src/cs/LionWeb.Integration.WebSocket.Server/WebSocketServer.cs
+++ src/cs/LionWeb.Integration.WebSocket.Server/WebSocketServer.cs
@@ -49,30 +49,25 @@ public class WebSocketServer : IDeltaRepositoryConnector
         var port = args.Length > 0
             ? int.Parse(args[0])
             : 40000;
-        
-        var testPartition = args.Length > 1
-            ? args[1]
-            : throw new ArgumentException("Missing partitionType");
 
         LionWebVersions lionWebVersion = LionWebVersions.v2023_1;
-    List<Language> languages =
-        [TestLanguageLanguage.Instance, lionWebVersion.BuiltIns, lionWebVersion.LionCore];
+        List<Language> languages =
+            [TestLanguageLanguage.Instance, lionWebVersion.BuiltIns, lionWebVersion.LionCore];
 
         var webSocketServer = new WebSocketServer(lionWebVersion)
         {
             Languages = languages
         };
-        
+
         webSocketServer.StartServer(IpAddress, port);
 
         IPartitionInstance serverPartition = languages
             .SelectMany(l => l.Entities)
             .OfType<Concept>()
             .Where(c => c.Partition)
-            .Where(c => c.Name == testPartition)
             .Select(c => (IPartitionInstance)c.GetLanguage().GetFactory().CreateNode("a", c))
             .First();
-        
+
         var serverForest = new Forest();
         // var serverPartition = new DynamicPartitionInstance("a", ShapesLanguage.Instance.Geometry);
         // var serverPartition = new LenientPartition("a", webSocketServer.LionWebVersion.BuiltIns.Node);
@@ -227,7 +222,7 @@ public class WebSocketServer : IDeltaRepositoryConnector
             return "participation" + _nextParticipationId++;
         }
     }
-    
+
     private static void Log(string message, bool header = false) =>
         Console.WriteLine(header
             ? $"{ILionWebRepository.HeaderColor_Start}{message}{ILionWebRepository.HeaderColor_End}"
