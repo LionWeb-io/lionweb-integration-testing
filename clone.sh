ec8dda6963c2b14809dc1a6b167243a4428583ea
Meinte Boersma <meinte.boersma@gmail.com>
2026-06-26 10:48:03 +0200

    just use a script for cloning

--- clone.sh
+++ clone.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -e
 
-deno task clone
+rm -rf ../repos
+mkdir ../repos
+git clone --depth 1 https://github.com/LionWeb-io/lionweb-jvm ../repos/lionweb-jvm
+git clone --depth 1 https://github.com/LionWeb-io/specification ../repos/specification
+git clone --depth 1 https://github.com/LionWeb-io/lionweb-mps ../repos/lionweb-mps
+git clone --depth 1 https://github.com/LionWeb-io/lionweb-typescript ../repos/lionweb-typescript
 
