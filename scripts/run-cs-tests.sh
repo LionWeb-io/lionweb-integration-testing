03134b2d5e2d5ded89c78f15a633a8bf8c688d4e
Meinte Boersma <meinte.boersma@gmail.com>
2026-06-29 14:37:40 +0200

    ensure logs/ dir. exists

--- scripts/run-cs-tests.sh
+++ scripts/run-cs-tests.sh
@@ -7,6 +7,7 @@ cd cs
 echo "Building (from clean) and running WebSocket tests with configuration=$CONFIGURATION."
 dotnet clean
 dotnet build --configuration $CONFIGURATION "./$TEST_PROJECT/"
+mkdir -p "$TEST_PROJECT/logs"
 dotnet test --configuration $CONFIGURATION --logger trx "./$TEST_PROJECT/" > "$TEST_PROJECT/logs/test-$CONFIGURATION.log"
 cd ..
 
